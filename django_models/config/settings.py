fc6bb6ef10ef71e88ac4976a6f5f4e7aced9d72c
blabi <bbielski@lyck.pl>
2022-10-08 21:34:15 +0200

    some new features in models, templates and views

--- django_models/config/settings.py
+++ django_models/config/settings.py
@@ -135,10 +135,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# log all SQL snippet
-LOGGING = {
-    "version": 1,
-    "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "loggers": {"django.db.backends": {"level": "DEBUG"}},
-    "root": {"handlers": ["console"]},
-}
+# # log all SQL snippet
+# LOGGING = {
+#     "version": 1,
+#     "handlers": {"console": {"class": "logging.StreamHandler"}},
+#     "loggers": {"django.db.backends": {"level": "DEBUG"}},
+#     "root": {"handlers": ["console"]},
+# }
--- django_models/store/urls.py
+++ django_models/store/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('category/<str:name>', views.category_view, name='category')
+    path('category/<str:name>', views.category_view, name='category'),
+    path('alamakots/<int:pk>', views.DetailProductView.as_view(), name='product-detail'),
 ]
